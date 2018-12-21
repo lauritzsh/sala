8853552d9dca05b12e6afa5a2fd9de5eaa01451e
Lauritz Hilsøe <mail@lauritz.me>
2018-12-21 21:29:20 +0100

    Emojis

--- client/src/Avatar.js
+++ client/src/Avatar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { SSL_OP_CRYPTOPRO_TLSEXT_BUG } from 'constants';
 
 const Avatar = ({ left, symbol, style, big }) => (
   <div style={{ ...style, fontSize: big ? '2rem' : '0.5rem' }}>
