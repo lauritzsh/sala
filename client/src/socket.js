1b0e2a324356276ea2c78b3e58c5060d1b2d6cfb
Lauritz Hilsøe <mail@lauritz.me>
2019-01-09 19:11:47 +0100

    wss

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,9 @@
 import { Socket } from 'phoenix';
 
-const socketUrl = process.env.NODE_ENV === 'production'
-  ? 'ws://sala.lauritz.me/socket'
-  : 'ws://localhost:4000/socket'
+const socketUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'wss://sala.lauritz.me/socket'
+    : 'ws://localhost:4000/socket';
 
 const socket = new Socket(socketUrl, {});
 socket.connect();
