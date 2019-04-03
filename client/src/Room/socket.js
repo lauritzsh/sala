31641036761019f1672cda87cbb206cd40985a07
Lauritz Hilsøe <mail@lauritz.me>
2019-04-03 18:19:01 +0200

    ayy

--- client/src/Room/socket.js
+++ client/src/Room/socket.js
@@ -3,7 +3,7 @@ import { Socket } from 'phoenix';
 const socketUrl =
   process.env.NODE_ENV === 'production'
     ? 'wss://sala.lauritz.me/socket'
-    : 'ws://localhost:4000/socket';
+    : 'wss://86a77cd5.ngrok.io/socket'; // 'ws://localhost:4000/socket';
 
 const socket = new Socket(socketUrl, {});
 socket.connect();
