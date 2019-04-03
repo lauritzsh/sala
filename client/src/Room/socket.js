da6e0509ca2cf789184ccf9465b3e464610042c5
Lauritz Hilsøe <mail@lauritz.me>
2019-04-03 19:31:45 +0200

    new domain on prod

--- client/src/Room/socket.js
+++ client/src/Room/socket.js
@@ -2,8 +2,8 @@ import { Socket } from 'phoenix';
 
 const socketUrl =
   process.env.NODE_ENV === 'production'
-    ? 'wss://sala.lauritz.me/socket'
-    : 'wss://melodic-elliptical-starling.gigalixirapp.com/socket'; // 'ws://localhost:4000/socket';
+    ? 'wss://melodic-elliptical-starling.gigalixirapp.com/socket'
+    : 'ws://localhost:4000/socket';
 
 const socket = new Socket(socketUrl, {});
 socket.connect();
