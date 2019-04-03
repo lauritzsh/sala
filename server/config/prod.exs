17a335054ba5bf5464fb787e6bc097a6a36653f6
Lauritz Hilsøe <mail@lauritz.me>
2019-04-03 18:21:39 +0200

    add gigalixir

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -72,3 +72,9 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
 # import_config "prod.secret.exs"
+
+config :sala, SalaWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
